692a31472d7f9e6c1aa754461188e6588174b976
Star_dusted <zhaotianxion@gmail.com>
2022-05-25 11:23:16 +0800

    first version

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const getValueByKeyInArray = (
+    arr,
+    key,
+    config = {
+      keyPropertyName: "key",
+      valuePropertyName: "label",
+    }
+  ) => {
+    const { keyPropertyName, valuePropertyName } = config;
+  
+    const findItem = arr.find((item) => item[keyPropertyName] === key);
+  
+    if (findItem) {
+      return findItem[valuePropertyName];
+    }
+    return "";
+  };
+
+exports.getValueByKeyInArray = getValueByKeyInArray;
